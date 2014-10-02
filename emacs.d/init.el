27822f509f74b6304403ef6c2b32f07dc3651a5e
gogotanaka <mail@tanakakazuki.com>
2014-10-02 15:20:11 -0700

    Emacs basis setting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,16 +1,35 @@
 ;Basis
-(show-paren-mode t)
+
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+(require 'install-elisp)
+(setq install-elisp-repository-directory "~/.emacs.d/elisp/")
+
+(set-default-coding-systems 'utf-8)
 (setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+(setq makebackup-files nil)
 (setq frame-title-fomat "%f")
 (setq inhibit-startup-message t)
+(setq completion-ignore-case t)
+
+(show-paren-mode)
+(show-paren-mode t)
+
 (set-face-background 'default "black")
 (set-face-foreground 'default "green")
 (set-face-background 'region "white")
 (set-face-foreground 'region "black")
 (set-frame-parameter nil 'alpha 75)
 
+(global-set-key "\C-h" 'delete-backward-char)
+;(global-set-key "\C-m" 'newline-and-indent)
+(global-set-key  "\C-j" 'newline)
 
-(set-frame-font "Ricty 24" nil t)
+; same for M-x linum-mode
+(global-linum-mode t)
+
+(global-auto-revert-mode 1)
 
 
 (defun my/insert-line-before ()
@@ -22,3 +41,23 @@
 
 (global-set-key (kbd "C-S-o")
                 "my/insert-line-before")
+
+;; Marmalade setting
+(require 'package)
+(add-to-list 'package-archives
+                 '("marmalade" .
+                         "http://marmalade-repo.org/packages/"))
+
+; Highlight current row
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     (:background "dark slate gray"))
+    (((class color)
+      (background light))
+     (:background  "#98FB98"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+(global-hl-line-mode)
