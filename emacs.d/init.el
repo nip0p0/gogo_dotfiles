97a2abc71fd406a6130322b90612d1684676b96a
gogotanaka <mail@tanakakazuki.com>
2014-10-03 10:43:42 -0700

    User elpa instead of elisp

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,12 +1,33 @@
 ; Third party
 ;; install-elisp
-(setq load-path (cons "~/.emacs.d/elisp" load-path))
-(require 'install-elisp)
-(setq install-elisp-repository-directory "~/.emacs.d/elisp/")
+;(setq load-path (cons "~/.emacs.d/elisp" load-path))
+;(require 'install-elisp)
+;(setq install-elisp-repository-directory "~/.emacs.d/elisp/")
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
 
 ;; auto-complete
 (require 'auto-complete)
 (global-auto-complete-mode t)
+
+;; key-combo
+(require 'key-combo)
+(key-combo-mode 1)
+(global-key-combo-mode t)
+(key-combo-define-global (kbd "=") '(" = " " == " " === " ))
+(key-combo-define-global (kbd "=>") " => ")
+(key-combo-define-global (kbd ">") '(" > " " => "))
+(key-combo-define-global (kbd "<") '(" < " " =< "))
+
+
+(require 'helm-config)
+(helm-mode 1)
+
+
+;(require 'ruby-electric)
 
 ; Basis
 (set-default-coding-systems 'utf-8)
@@ -36,6 +57,8 @@
 
 (global-auto-revert-mode 1)
 
+;; Delete trailing whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (defun my/insert-line-before ()
   "Insert a newline above the line containing the cursor"
@@ -44,7 +67,7 @@
     (move-beginning-of-line 1)
 		(newline)))
 
-(global-set-key (kbd "C-S-o")
+(global-set-key (kbd "\C-O")
                 "my/insert-line-before")
 
 ; Highlight current row
