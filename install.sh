de73b3903f04636e76a4244d3af71af14cea1df6
gogotanaka <mail@tanakakazuki.com>
2015-06-04 11:56:21 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,23 @@
 #!/bin/sh
 
 # make symbolic links
-
-for name in *; do
-  if [ "$name" != "vim_color" ]; then
-    target="$HOME/.$name"
-    if [ -e "$target" ]; then
-      if [ ! -L "$target" ]; then
-        echo "WARNING: $target exists but is not a symlink."
-      fi
-    else
-      if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ]; then
-        echo "Creating $target"
-        ln -s "$PWD/$name" "$target"
-      fi
+for name in home/*; do
+  target=$HOME/.`basename $name`
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
     fi
+  else
+    ln -s "$PWD/$name" "$target"
   fi
 done
 
-# Install vim color scheme
-mkdir -p ~/.vim/colors/
-cp ./vim_color/railscasts.vim ~/.vim/colors/
-
-# Install NeoBundle
-if [ ! -d $HOME/.vim/bundle/neobundle.vim ]
-then
-  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-fi
+ # Install vim color scheme
+ mkdir -p ~/.vim/colors/
+ cp ./vim_color/railscasts.vim ~/.vim/colors/
+ 
+ # Install NeoBundle
+ if [ ! -d $HOME/.vim/bundle/neobundle.vim ]
+ then
+   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+ fi
